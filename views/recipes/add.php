856ca70307360a5f736a44a53bb95a30a421b02c
Bastien Lalos <bastien.lalos@gmail.com>
2017-02-02 00:04:40 +0100

    fix add recipe

--- views/recipes/add.php
+++ views/recipes/add.php
@@ -1,4 +1,4 @@
-<div class="content-wrapper">
+<div class="content-wrapper" style="background : url(assets/hero-bg-1.jpg);background-size:cover cover;" >
 
   <div class="page-intro-box">
       <div class="box-inner">
@@ -14,27 +14,93 @@
   </div>
 
 
-
-  <div style="margin-bottom:10%;"class="container">
-    <form class="contact-form">
+  <div class="container">
+    <form method="get" style="margin-bottom:25%;" class="contact-form">
         <h1 class="form-title">Alors vous êtes fin cuisinier ?</h1>
         <p class="form-subtitle">Laissez nous votre nom ainsi que votre e-mail afin que nous puissions vous prévenir de la validation de votre recettes. </p>
         <div class="input-line">
-            <input type="text" class="form-input check-value" />
+            <input type="text" name="name" class="form-input check-value" />
             <span class="input-placeholder">Nom</span>
         </div>
 
         <div class="input-line">
-            <input type="text" class="form-input check-value" />
-            <span class="input-placeholder">Email</span>
+            <input type="text" name="email" class="form-input check-value" />
+            <span class="input-placeholder">Adresse e-mail</span>
         </div>
 
         <div class="input-line">
-            <input type="text" class="form-input check-value" />
+            <input type="text" name="title" class="form-input check-value" />
             <span class="input-placeholder">Titre de la recette</span>
         </div>
+        <div class="input-line">
+            <textarea type="text" name="description" class="form-input check-value"> </textarea>
+            <span class="input-placeholder">Description</span>
+        </div>
+
         <div class="row">
-          <legend>Ingredients:</legend>
+            <div class="col-md-6">
+                <div class="recipes-filters-block">
+                    <h4 style="color:white;" class="block-title primary-font">Catégorie(s)</h4>
+
+                    <ul class="clean-list recipe-categories">
+                        <li class="recipe-category">
+                            <div class="check-box">
+                                <label>
+                                    <input type="checkbox" name="category-meat" class="check-box-input" />
+                                    <span style="color:#f6c935;" class="author check-box-title">Viande</span>
+                                </label>
+                            </div>
+                        </li>
+
+                        <li class="recipe-category">
+                            <div class="check-box">
+                                <label>
+                                    <input type="checkbox" name="category-pizza" class="check-box-input" />
+                                    <span style="color:#f6c935;" class="check-box-title">Pizza</span>
+                                </label>
+                            </div>
+                        </li>
+
+                        <li class="recipe-category">
+                            <div class="check-box">
+                                <label class="box-label">
+                                    <input type="checkbox" name="category-deserts" class="check-box-input" />
+                                    <span style="color:#f6c935;" class="check-box-title">Desserts</span>
+                                </label>
+                            </div>
+                        </li>
+
+                        <li class="recipe-category">
+                            <div class="check-box">
+                                <label class="box-label">
+                                    <input type="checkbox" name="category-salads" class="check-box-input" />
+                                    <span style="color:#f6c935;" class="check-box-title">Légumes</span>
+                                </label>
+                            </div>
+                        </li>
+
+                        <li class="recipe-category">
+                            <div class="check-box">
+                                <label class="box-label">
+                                    <input type="checkbox" name="category-fish" class="check-box-input" />
+                                    <span style="color:#f6c935;" class="check-box-title">Poisson</span>
+                                </label>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+              <div class="recipes-filters-block">
+                  <h4 style="color:white;" class="block-title primary-font">Temps de préparation (minutes)</h4>
+                  <input id="timeinput" min = 0 max = 120 name="time" type="number" placeholder="5 minutes" class="form-control input-md">
+              </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <h4 style="color:white;" class="block-title primary-font">Ingrédient(s)</h4>
           <fieldset>
             <div id="dynamicInput">
               <div class="form-group">
@@ -42,10 +108,10 @@
                   <input id="textinput" name="noms[]" type="text" placeholder="Nom" class="form-control input-md">
                 </div>
                 <div class="col-md-3">
-                  <input id="textinput" name="quantites[]" type="text" placeholder="Quantité" class="form-control input-md">
+                  <input id="textinput" name="quantites[]" type="number" placeholder="Quantité" class="form-control input-md">
                 </div>
                 <div class="col-md-3">
-                  <select id="selectbasic" name="unites[]" class="form-control">
+                  <select id="selectbasic" name="unite[]" class="form-control">
                     <option value="g">g</option>
                     <option value="mL">mL</option>
                     <option value="L">L</option>
@@ -54,14 +120,15 @@
                   </select>
                 </div>
                 <div class="col-md-3">
-                  <button class="btn btn-primary" type="button" onClick="addInput('dynamicInput');">Add</button>
+                  <button class="btn btn-warning" type="button" onClick="addInput('dynamicInput');"><i class="fa fa-plus"></i></button>
                 </div>
               </div>
             </div>
           </fieldset>
         </div>
+        <br>
         <div class="row">
-          <legend>Etapes:</legend>
+            <h4 style="color:white;" class="block-title primary-font">Étape(s)</h4>
           <fieldset>
             <div id="dynamicSteps">
               <div class="form-group">
@@ -69,15 +136,15 @@
                     <textarea class="form-input check-value"></textarea>
                 </div>
                 <div class="col-md-2" style="margin-left: 10%; margin-top: 5%;">
-                  <button class="btn btn-primary" type="button" onClick="addStep('dynamicSteps');">Add</button>
+                  <button class="btn btn-warning" type="button" onClick="addStep('dynamicSteps');"><i class="fa fa-plus"></i></button>
                 </div>
               </div>
             </div>
           </fieldset>
         </div>
-        <span class="btn-wrapper align-center">
-            <button class="submit-button btn btn-default">
-                <span class="text">Send message</span>
+        <span class="btn-wrapper align-center"><br>
+            <button style="background: -webkit-linear-gradient(left,#f58a00 0,#f6d640 100%);" class="submit-button btn btn-simplified">
+                <span class="text">Ajouter la recette</span>
             </button>
         </span>
     </form>
